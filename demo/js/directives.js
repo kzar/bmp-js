1258ab30ada5e2defc229d6618112936ded1ff62
David Barker <kzar@kzar.co.uk>
2013-10-10 22:26:32 +0100

    Removed some of the console logging.

--- demo/js/directives.js
+++ demo/js/directives.js
@@ -15,7 +15,7 @@
                 height = parseInt(attrs.h),
                 channels = parseInt(attrs.channels) / 8;
             if (lines) {
-              console.log("DRAWING", width, height, channels, lines.length, lines[0]);
+              console.log("DRAWING", width, height, channels, lines.length);
               // Reset the canvas
               elm[0].width = width;
               elm[0].height = height;
@@ -24,10 +24,8 @@
               for (y = 0; y < height; y +=1) {
                 for (x = 0; x < width; x +=1) {
                   for (i = 0; i < channels; i += 1) {
-                    console.log((x + y * width) * 4 + i, lines[y][x] >> (8 * i) & 0xff);
                     image_data[(x + y * width) * 4 + i] = lines[y][x] >> (8 * i) & 0xff;
                   }
-                  image_data[(x + y * width) * 4 + 3] = 255; // FIXME bodged alpha for now!
                 }
               }
               console.log(image_data);
